69c09de5dbc4f773cc0275c4fd458174974cae16
David Liu <guangyuanliu@gmail.com>
2018-09-20 22:54:53 -0400

    Dependency Injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import {ProductListComponent} from './products/product-list.component';
 import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductService } from './products/product.service';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { StarComponent } from './shared/star.component';
   imports: [
     BrowserModule, FormsModule
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [ProductService]
 })
 export class AppModule { }
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
@@ -7,11 +8,17 @@ import { IProduct } from "./product";
     styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit, OnDestroy{
+   
     pageTitle: string = 'Product List!!';
     imageWidth: number = 50;
     imageMargin: number=2;
     imageDisplay: boolean = false;
     _listFilter: string;
+    
+
+    constructor(private productService: ProductService){
+    }
+
     get listFilter(): string{
         return this._listFilter;
     }
@@ -21,10 +28,6 @@ export class ProductListComponent implements OnInit, OnDestroy{
 
     }
 
-    constructor(){
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart'; // this cause
-    }
     performFilter(filterBy: string):IProduct[]{
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product: IProduct) =>
@@ -37,34 +40,15 @@ export class ProductListComponent implements OnInit, OnDestroy{
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[]=[
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-          },
-          {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-          },
-    ];
+    products: IProduct[];
 
     toggleImage(): void{
         this.imageDisplay = !this.imageDisplay;
     }
 
     ngOnInit():void{
+        this.products = this.productService.getProduct();
+        this.filteredProducts = this.products;
         console.log('In OnInit');
     }
 
