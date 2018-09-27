4ee8b6d1eb58007d2d581c4892d38ca412d1e9b8
David Liu <guangyuanliu@gmail.com>
2018-09-27 07:08:17 -0400

    Angular Modules - create Share Module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/star.component';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
     RouterModule.forChild(
       [
         {path: 'products', component: ProductListComponent},
@@ -20,13 +17,12 @@ import { ProductDetailGuard } from './product-detail.guard';
         },
       ]
     ),
-    FormsModule
+    SharedModule
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ConvertToSpacesPipe,
-    StarComponent
+    ConvertToSpacesPipe
   ]
 })
 export class ProductModule { }
